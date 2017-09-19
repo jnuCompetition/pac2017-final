6455d61d6ce499a8f25a67d802382a9797ef0ea1
zhpmatrix <zhpmatrix@gmail.com>
2017-09-20 01:41:21 +0800

    add readData

--- main.py
+++ main.py
@@ -3,14 +3,29 @@ import pandas as pd
 
 def saveData(data, path):
     data.to_csv(path, index=False, encoding='utf-8')
-def readData(fileDir):
+
+def readTest(fileDir):
+    data = pd.DataFrame()
+    for filename in os.listdir(fileDir):
+        _data = pd.read_excel(fileDir+filename, header=None, skiprows=[0])
+        data = data.append(_data.ix[:, [1,2,3]], ignore_index=False)
+    saveData(data, path='../test.csv')
+    return data
+
+def readTrain(fileDir):
     data = pd.DataFrame()
     for filename in os.listdir(fileDir):
         _data = pd.read_excel(fileDir+filename, header=None, skiprows=[0,1])
-        data = data.append(_data.ix[:, [0,8]], ignore_index=False)
-    saveData(data, path='data.csv')
+        data = data.append(_data.ix[:, [0,1,8]], ignore_index=False)
+    saveData(data, path='../train.csv')
     return data
+
 if __name__ == '__main__':
-    fileDir = 'data/'
-    data = readData(fileDir)
-    print(data.shape)
+
+    fileDir = '../train/'
+    train = readTrain(fileDir)
+    print(train.shape)
+    
+    fileDir = '../test/'
+    test = readTest(fileDir)
+    print(test.shape)
